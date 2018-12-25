8a24fe71a421abd9974ed1b9f5f452c988800b39
argot <pablotesari@gmail.com>
2018-12-25 17:29:25 -0300

    Sign in activity

--- app/src/main/java/com/example/uguu_uploader/LoginActivity.java
+++ app/src/main/java/com/example/uguu_uploader/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -14,12 +15,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         Button btnLogin = findViewById(R.id.btnLogin);
+        TextView tvLoginSignin = findViewById(R.id.tvLoginSignin);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 /* TODO LOGIN */
                 Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        tvLoginSignin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignInActivity.class);
                 startActivity(i);
             }
         });
