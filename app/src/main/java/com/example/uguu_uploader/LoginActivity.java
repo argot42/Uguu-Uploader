0ef91266b8104272cba1206cf849a11c2a339d3e
argot <pablotesari@gmail.com>
2018-12-25 17:02:36 -0300

    Basic activities

--- app/src/main/java/com/example/uguu_uploader/LoginActivity.java
+++ app/src/main/java/com/example/uguu_uploader/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.uguu_uploader;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnLogin = findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /* TODO LOGIN */
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
