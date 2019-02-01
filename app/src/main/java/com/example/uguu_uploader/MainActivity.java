0d9ff92c06936383c062102c3d44572eb4b744cb
argot <pablotesari@gmail.com>
2019-02-01 08:15:33 -0300

    Camera support

--- app/src/main/java/com/example/uguu_uploader/MainActivity.java
+++ app/src/main/java/com/example/uguu_uploader/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.uguu_uploader;
 
+import android.Manifest;
 import android.content.ContentResolver;
 import android.content.Intent;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
-import android.provider.OpenableColumns;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +20,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.webkit.URLUtil;
+import android.net.Uri;
 
 import com.android.volley.NetworkResponse;
 import com.android.volley.NoConnectionError;
@@ -29,25 +35,35 @@ import com.example.uguu_uploader.dao.UguuDatabase;
 import com.example.uguu_uploader.model.Upload;
 import com.example.uguu_uploader.request.MultipartRequest;
 
-import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static android.os.Environment.getExternalStoragePublicDirectory;
+
 public class MainActivity extends AppCompatActivity {
 
     private static int NEWUPLOAD = 0;
+    private static int NEWPHOTO = 1;
+
+    private static int WRITE_EXTERNAL_STORAGE_REQUEST = 0;
+
     private List<Upload> uploads = new ArrayList<>();
     private UploadAdapter uploadAdapter = new UploadAdapter(uploads);
 
+    private String username;
+
+    private Uri photoURI = null;
+
     private UguuDatabase db = UguuDatabase.getDatabase(this);
 
-    //private static String url = "http://172.16.0.2/api.php?d=upload-tool"; // DEBUG
-    private static String url = "http://192.168.0.12/api.php?d=upload-tool"; // DEBUG
+    private static String url = "http://172.16.0.2/api.php?d=upload-tool"; // DEBUG
+    //private static String url = "http://192.168.0.12/api.php?d=upload-tool"; // DEBUG
     //private static String url = "https://uguu.se/api.php?d=upload-tool";
 
     @Override
@@ -57,13 +73,14 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
 
         Intent intent = getIntent();
-        final String username = intent.getStringExtra("username");
+        username = intent.getStringExtra("username");
         toolbar.setTitle("Uguu Uploader | " + username);
 
         setSupportActionBar(toolbar);
 
         setUpRecyclerView(username);
 
+        // Open upload activity
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -71,6 +88,35 @@ public class MainActivity extends AppCompatActivity {
                 Intent i = new Intent(MainActivity.this, UploadActivity.class);
                 i.putExtra("username", username);
                 startActivityForResult(i, NEWUPLOAD);
+            }
+        });
+
+        // Open camera
+        fab.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (i.resolveActivity(getPackageManager()) == null) {
+                    return false;
+                }
+                File photoFile;
+                try {
+                    photoFile = createImageFile();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    return false;
+                }
+                /*if (photoFile == null) {
+                    return false;
+                }*/
+                photoURI = FileProvider.getUriForFile(
+                        getApplicationContext(),
+                        "com.example.uguu_uploader.fileprovider",
+                        photoFile
+                );
+                i.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(i, NEWPHOTO);
+                return true;
             }
         });
 
@@ -111,6 +157,20 @@ public class MainActivity extends AppCompatActivity {
                 uploadAdapter.addItem(u);
                 upload(u);
             }
+        }
+        else if (requestCode == NEWPHOTO && resultCode == RESULT_OK) {
+            Log.d("Picture", "Picture taken!");
+
+            Upload u = new Upload();
+            u.setUri(photoURI);
+            u.setUser(username);
+            u.setName(u.queryName(getContentResolver()));
+            u.setRandomfilename(true);
+            u.setCustomName("");
+
+            save(u);
+            uploadAdapter.addItem(u);
+            upload(u);
         }
     }
 
@@ -211,5 +271,17 @@ public class MainActivity extends AppCompatActivity {
         };
         Thread t = new Thread(r);
         t.start();
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "picture_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        Log.d("createImageFile", storageDir.getPath());
+        return File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
     }
 }
